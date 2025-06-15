36e525898023b0deba97accce9f926135287b090
kaanmetep <kaanpmete@gmail.com>
2025-06-15 23:01:53 +0300

    test cors settings

--- backend/api/index.js
+++ backend/api/index.js
@@ -29,6 +29,17 @@ app.use(cors(corsOptions));
 // Pre-flight istekleri için
 app.options("*", cors(corsOptions));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://findtheoneai.vercel.app");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,DELETE,OPTIONS,PATCH"
+  );
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 app.use(express.json());
 
 mongoose
