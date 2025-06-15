027187cbfb4a1696b92f67e256c886f9525dfbba
kaanmetep <kaanpmete@gmail.com>
2025-06-15 23:46:51 +0300

    add 18 age limit

--- frontend/src/features/authentication/register/validation/step3Validation.jsx
+++ frontend/src/features/authentication/register/validation/step3Validation.jsx
@@ -4,16 +4,27 @@ const step3Validation = yup.object({
   firstName: yup.string().required("Please enter your name."),
   birthdayDate: yup
     .date()
-    .typeError("Please enter a valid date.") // todo: check if user is older than 18
+    .typeError("Please enter a valid date.")
     .required("Please enter your birthday date.")
     .test(
       "is-year-valid",
       "The year must not be greater than 2024.",
       (value) => {
-        if (!value) return false; // Eğer tarih boşsa
+        if (!value) return false;
         return value.getFullYear() <= 2024;
       }
-    ),
+    )
+    .test("is-18-or-older", "You must be at least 18 years old.", (value) => {
+      if (!value) return false;
+      const today = new Date();
+      const birthDate = new Date(value);
+      let age = today.getFullYear() - birthDate.getFullYear();
+      const m = today.getMonth() - birthDate.getMonth();
+      if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+      }
+      return age >= 18;
+    }),
   gender: yup.string().required("Please choose your gender."),
 
   occupation: yup.string().required("Please enter your occupation."),
